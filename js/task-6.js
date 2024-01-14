0e96c0023cc821bf37055667001d29c41909e62a
RomanMazuraka <144357987+RomanMazuraka@users.noreply.github.com>
2024-01-14 17:31:54 +0100

    goit-js-hw-07-done

--- js/task-6.js
+++ js/task-6.js
@@ -1,5 +1,43 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
+    .padStart(6, '0')}`;
 }
+
+  const input = document.querySelector('input');
+  const createButton = document.querySelector('[data-create]');
+  const destroyButton = document.querySelector('[data-destroy]');
+  const boxesContainer = document.getElementById('boxes');
+
+  createButton.addEventListener('click', createBoxes);
+  destroyButton.addEventListener('click', destroyBoxes);
+
+  function createBoxes() {
+    const amount = Number(input.value);
+
+    if (amount < 1 || amount > 100) {
+      return;
+    }
+
+    boxesContainer.innerHTML = '';
+
+    let size = 30;
+
+    for (let i = 0; i < amount; i++) {
+      const box = document.createElement('div');
+      box.classList.add('box');
+      box.style.width = `${size}px`;
+      box.style.height = `${size}px`;
+      box.style.backgroundColor = getRandomHexColor();
+
+      boxesContainer.appendChild(box);
+
+      size += 10;
+    }
+
+    input.value = '';
+  }
+
+  function destroyBoxes() {
+    boxesContainer.innerHTML = '';
+  };
