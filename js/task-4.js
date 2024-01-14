bbdeb5ae0703146b5b1d033f56a1143294a972de
RomanMazuraka <144357987+RomanMazuraka@users.noreply.github.com>
2024-01-14 11:54:41 +0100

    goit-js-hw-07-task5-done

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,22 @@
+const loginForm = document.querySelector('.login-form');
+loginForm.addEventListener('submit', function (event) {
+    event.preventDefault();
 
+    const formData = {};
+    for (let i = 0; i < loginForm.elements.length; i++) {
+      const element = loginForm.elements[i];
+      if (element.type !== 'submit') {
+        formData[element.name] = element.value.trim();
+      }
+    }
+
+    const formValues = Object.values(formData);
+    if (formValues.some(value => value === '')) {
+      alert('All form fields must be filled in');
+      return;
+    }
+
+    console.log(formData);
+
+    loginForm.reset();
+  });
